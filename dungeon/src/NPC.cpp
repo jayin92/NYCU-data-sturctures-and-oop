6e2b6bc2d052eec74f938fbfdf37364cd36f7a9e
jayin92 <jayin920805@gmail.com>
2022-03-27 13:13:15 +0800

    all primitive object implementation done

--- dungeon/src/NPC.cpp
+++ dungeon/src/NPC.cpp
@@ -1,2 +1,28 @@
 #include "NPC.h"
 
+NPC::NPC(string name, string script, vector<Item> com):
+    GameCharacter(name, "NPC", 0, 0, 0), script(script), commodity(com) {}
+
+void NPC::listCommodity(){
+    for(auto i: this->commodity){
+        std::cout << i.getName() << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+void NPC::setScript(string script){
+    this -> script = script;
+}
+
+void NPC::setCommodity(vector<Item> com){
+    this -> commodity = com;
+}
+
+string NPC::getScript(){
+    return this -> script;
+}
+
+vector<Item> NPC::getCommodity(){
+    return this -> commodity;
+}
--- dungeon/src/Room.cpp
+++ dungeon/src/Room.cpp
@@ -1,1 +1,62 @@
 #include "Room.h"
+
+Room::Room(bool isExit, int idx, vector<Object*> objs){
+    this -> isExit = isExit;
+    this -> index = idx;
+    this -> objects = objs;
+}
+
+bool Room::popObject(Object* obj){
+    for(auto i: this->objects){
+        if(i == obj){
+            this -> objects.erase(i);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void Room::setUpRoom(Room* r){
+    this -> upRoom = r;
+}
+
+void Room::setDownRoom(Room* r){
+    this -> downRoom = r;
+}
+
+void Room::setLeftRoom(Room* r){
+    this -> leftRoom = r;
+}
+
+void Room::setRightRoom(Room* r){
+    this -> rightRoom = r;
+}
+
+void Room::setIsExit(bool isExit){
+    this -> isExit = isExit;
+}
+
+void Room::setIndex(int idx){
+    this -> index = idx;
+}
+
+vector<Object*> Room::getObjects(){
+    return this -> objects;
+}
+
+Room* Room::getUpRoom(){
+    return this -> upRoom;
+}
+
+Room* Room::getDownRoom(){
+    return this -> downRoom;
+}
+
+Room* Room::getLeftRoom(){
+    return this -> leftRoom;
+}
+
+Room* Room::getRightRoom(){
+    return this -> rightRoom;
+}
